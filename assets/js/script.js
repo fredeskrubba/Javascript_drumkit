69789d07f18949babf70e7b6a6db758a19c15a43
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-02-12 15:47:06 +0100

    Finished JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,18 +13,46 @@ document.addEventListener("keypress", (e)=>{
 
 function sound(key){
     switch(key){
+        // switch fallthrough cause || doesn't work in a switch
         case "q":
         case "Q":
-            let sound1 = new Audio("air.mp3");
-                sound1.play;
-                break;
-        case "W":
-            let sound2 = new Audio("clap.wav");
-            sound2.play;
+            let sound1 = new Audio("assets/sounds/boom.wav");
+            sound1.play();
             break;
+        case "w":
+        case "W":
+            let sound2 = new Audio("assets/sounds/clap.wav");
+            sound2.play();
+            break;
+        case "e":
         case "E":
-            let sound3 = new Audio("hihat.wav");
-            sound3.play;
+            let sound3 = new Audio("assets/sounds/hihat.wav");
+            sound3.play();
+            break;
+        case "r":
+        case "R":
+            let sound4 = new Audio("assets/sounds/kick.wav");
+            sound4.play();
+            break;
+        case "t":
+        case "T":
+            let sound5 = new Audio("assets/sounds/openhat.wav");
+            sound5.play();
+            break;
+        case "y":
+        case "Y":
+            let sound6 = new Audio("assets/sounds/ride.wav");
+            sound6.play();
+            break;
+        case "a":
+        case "A":
+            let sound7 = new Audio("assets/sounds/snare.wav");
+            sound7.play();
+            break;
+        case "s":
+        case "S":
+            let sound8 = new Audio("assets/sounds/tink.wav");
+            sound8.play();
             break;
     }   
 }
