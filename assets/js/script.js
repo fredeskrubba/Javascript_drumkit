821c4c8b1a4cc4943de8044b73652b97b3bbd9b1
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2022-02-12 15:20:42 +0100

    added features

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
+const buttons = document.querySelectorAll("button");
+const buttonArray = Array.from(buttons);
+
+buttonArray.forEach(button =>{
+    button.addEventListener("click", ()=>{
+        sound(button.innerHTML);
+    });
+});
+
+document.addEventListener("keypress", (e)=>{
+    sound(e.key);
+});
+
+function sound(key){
+    switch(key){
+        case "q":
+        case "Q":
+            let sound1 = new Audio("air.mp3");
+                sound1.play;
+                break;
+        case "W":
+            let sound2 = new Audio("clap.wav");
+            sound2.play;
+            break;
+        case "E":
+            let sound3 = new Audio("hihat.wav");
+            sound3.play;
+            break;
+    }   
+}
 
